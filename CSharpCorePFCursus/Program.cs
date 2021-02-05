4469522fe2b7770a981d31b5586ffe45f3d7afaa
YannickWillems <yannick.willems@vdabcampus.be>
2021-02-05 19:00:36 +0100

    Add project files.

---
+++ CSharpCorePFCursus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCorePFCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpCorePFOefenmap/01_VCB/ConversieTemperatuur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpCorePFOefenmap
+{
+    class Program
+    {
+        const float GemLichTempCelsius = 37.0F;
+        static void Main(string[] args)
+        {
+            float gemLichtTempFahrenheit =
+                GemLichTempCelsius * 9.0F / 5.0F + 32.0F;
+            Console.WriteLine("Gem. lichaamstemperatuur in graden " +
+                "Celsius: " + GemLichTempCelsius );
+            Console.WriteLine("Gem. lichaamstemperatuur in graden " +
+                "Fahrenheit: " + gemLichtTempFahrenheit );
+        }
+    }
+}
